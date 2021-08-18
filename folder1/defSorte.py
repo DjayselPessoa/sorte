2fc30ee5488e20a6c33b3e7287f76b4d1136ce2a
Djaysel Pessôa <djaysel@outlook.com>
2021-08-18 13:11:09 -0300

    Create defSorte.py

---
+++ folder1/defSorte.py
@@ -0,0 +1,9 @@
+def temp(extraTeste):
+    extraTeste = extraTeste
+    if extraTeste >= 50:
+        print("Está muito frio!")
+    elif 50 > extraTeste >= 30:
+        print("Está morno!")
+    elif 30 > extraTeste >= 1:
+        print("Está quente!")
+    return ""
