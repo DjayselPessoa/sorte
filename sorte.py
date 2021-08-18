8638538e17198c905d65de98064b5460c7344399
Djaysel Pessôa <djaysel@outlook.com>
2021-08-18 12:14:49 -0300

    Update sorte.py

--- sorte.py
+++ sorte.py
@@ -1,13 +1,14 @@
 import random
 
 
-valorResposta = random.randrange(0, 101) # o 0 é incluido mas o 101 não, indo somente até 100
+valorResposta = random.randrange(0, 101)
 valorResposta = int(valorResposta)
 # print(valorResposta)
 valorAntigo = 0
 exatoValor = True
+extraTeste = 0
 
-print("Melhor de três é o jogo que definirá o nível da sua sorte hoje!\nVocê tem 3 chances para advinhar o número certo que está entre 0 e 100!\nVamos lá:\n")
+print("Melhor de três é o jogo que definirá o nível da sua sorte hoje!\nVocê tem 3 chances para advinhar o número certo de 0 e 100!\nVamos lá:\n")
 chances = 1
 
 while chances < 4:
@@ -18,22 +19,38 @@ while chances < 4:
             raise ValueError("ERRO 1A")
         else:
             if valorJogador == valorResposta and chances == 1:
-                print("Seu nível de sorte está magnífico hoje! Número certo na primeira jogada! Parabéns e aproveite!")
+                print("Vitória!\nSeu nível de sorte está magnífico hoje! Número certo na primeira jogada! Parabéns e aproveite!")
                 break
             elif valorJogador == valorResposta and chances == 2:
-                print("Você pode até achar que não tem sorte, mas a verdade é que só precisou de duas tentativas para acertar! Parabéns!")
+                print("Vitória!\nVocê pode até achar que não tem sorte, mas a verdade é que só precisou de duas tentativas para acertar! Parabéns!")
                 break
             elif valorJogador == valorResposta and chances == 3:
-                print("Sua sorte não está na melhor forma, mas pense que só precisou de três tentativas! Parabéns!")
+                print("Vitória!\nSua sorte não está na melhor forma, mas pense que só precisou de três tentativas! Parabéns!")
                 break
             elif valorJogador != valorResposta and chances == 1:
                 print("Não se preocupe que você ainda possui duas tentativas!")
                 valorAntigo = valorJogador
                 if valorJogador < valorResposta:
                     print(f"O número está entre {valorJogador} e 100!")
+                    extraTeste = valorResposta - valorJogador
+                    # print(extraTeste)
+                    if extraTeste >= 50:
+                        print("Está muito frio!")
+                    elif 50 > extraTeste >= 30:
+                        print("Está morno!")
+                    elif 30 > extraTeste >= 1:
+                        print("Está quente!")
                     exatoValor = True
                 elif valorJogador > valorResposta:
                     print(f"O número está entre 0 e {valorJogador}!")
+                    extraTeste = valorJogador - valorResposta
+                    # print(extraTeste)
+                    if extraTeste >= 50:
+                        print("Está muito frio!")
+                    elif 50 > extraTeste >= 30:
+                        print("Está morno!")
+                    elif 30 > extraTeste >= 1:
+                        print("Está quente!")
                     exatoValor = False
                 chances = 2
                 raise ValueError("Número incorreto!")
@@ -42,19 +59,51 @@ while chances < 4:
                 if exatoValor:
                     if valorJogador < valorResposta:
                         print(f"O número está entre {valorJogador} e 100!")
+                        extraTeste = valorResposta - valorJogador
+                        # print(extraTeste)
+                        if extraTeste >= 50:
+                            print("Está muito frio!")
+                        elif 50 > extraTeste >= 30:
+                            print("Está morno!")
+                        elif 30 > extraTeste >= 1:
+                            print("Está quente!")
                     elif valorJogador > valorResposta:
                         print(f"O número está entre {valorAntigo} e {valorJogador}!")
+                        extraTeste = valorJogador - valorResposta
+                        # print(extraTeste)
+                        if extraTeste >= 50:
+                            print("Está muito frio!")
+                        elif 50 > extraTeste >= 30:
+                            print("Está morno!")
+                        elif 30 > extraTeste >= 1:
+                            print("Está quente!")
                 elif exatoValor == False:
                     if valorJogador < valorResposta:
                         print(f"O número está entre {valorJogador} e {valorAntigo}!")
+                        extraTeste = valorResposta - valorJogador
+                        # print(extraTeste)
+                        if extraTeste >= 50:
+                            print("Está muito frio!")
+                        elif 50 > extraTeste >= 30:
+                            print("Está morno!")
+                        elif 30 > extraTeste >= 1:
+                            print("Está quente!")
                     elif valorJogador > valorResposta:
                         print(f"O número está entre 0 e {valorJogador}!")
+                        extraTeste = valorJogador - valorResposta
+                        # print(extraTeste)
+                        if extraTeste >= 50:
+                            print("Está muito frio!")
+                        elif 50 > extraTeste >= 30:
+                            print("Está morno!")
+                        elif 30 > extraTeste >= 1:
+                            print("Está quente!")
                 chances = 3
                 raise ValueError("Número incorreto!")
             elif valorJogador != valorResposta and chances == 3:
-                print("Hoje é um dia em que sua sorte está dormindo! Mas lembre-se: o que chamamos por sorte é quando oportunidade e habilidade se encontram! Até a próxima!")
+                print("Hoje é um dia em que sua sorte está dormindo! Mas lembre-se: o que chamamos por sorte é quando oportunidade e habilidade se encontram!\nAté a próxima!")
                 print(f"O valor que o jogo escolheu foi {valorResposta}")
                 chances = 4
                 print("Fim do Jogo!")
     except ValueError as e:
-        print("ERRO - ", e)
+        print("FALHOU - ", e)
