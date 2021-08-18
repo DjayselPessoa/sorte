5e62c1933120f02dc5724d9a92f652210807aae0
Djaysel Pessôa <djaysel@outlook.com>
2021-08-18 18:33:19 -0300

    Update defSorteErro.py

--- folder1/defSorteErro.py
+++ folder1/defSorteErro.py
@@ -1,9 +1,9 @@
 from folder1.defSorte import temp
 
 
-def erro(chances, valorJogador, valorResposta):
+def erro(chances, valorJogador, valorResposta, valorAntigo):
     extraTeste = 0
-    valorAntigo = 0
+    valorAntigo = valorAntigo
     exatoValor = True
     valorJogador = valorJogador
     valorResposta = valorResposta
@@ -16,18 +16,20 @@ def erro(chances, valorJogador, valorResposta):
             print(f"O número está entre {valorJogador} e 100!")
             extraTeste = valorResposta - valorJogador
             # print(extraTeste)
+            print("ok1")
             print(temp(extraTeste))
             exatoValor = True
             chances = 2
-            return chances
+            return chances, valorAntigo
         elif valorJogador > valorResposta:
             print(f"O número está entre 0 e {valorJogador}!")
             extraTeste = valorJogador - valorResposta
             # print(extraTeste)
+            print("ok2")
             print(temp(extraTeste))
             exatoValor = False
             chances = 2
-            return chances
+            return chances, valorAntigo
     elif chances == 2:
         print("Não se preocupe que você ainda possui uma tentativa!")
         if exatoValor:
@@ -35,34 +37,38 @@ def erro(chances, valorJogador, valorResposta):
                 print(f"O número está entre {valorJogador} e 100!")
                 extraTeste = valorResposta - valorJogador
                 # print(extraTeste)
+                print("ok3")
                 print(temp(extraTeste))
                 chances = 3
-                return chances
+                return chances, valorAntigo
             elif valorJogador > valorResposta:
                 print(f"O número está entre {valorAntigo} e {valorJogador}!")
                 extraTeste = valorJogador - valorResposta
                 # print(extraTeste)
+                print("ok4")
                 print(temp(extraTeste))
                 chances = 3
-                return chances
+                return chances, valorAntigo
         elif exatoValor == False:
             if valorJogador < valorResposta:
                 print(f"O número está entre {valorJogador} e {valorAntigo}!")
                 extraTeste = valorResposta - valorJogador
                 # print(extraTeste)
+                print("ok5")
                 print(temp(extraTeste))
                 chances = 3
-                return chances
+                return chances, valorAntigo
             elif valorJogador > valorResposta:
-                print(f"O número está entre 0 e {valorJogador}!")
+                print(f"O número está entre {valorAntigo} e {valorJogador}!")
                 extraTeste = valorJogador - valorResposta
                 # print(extraTeste)
+                print("ok6")
                 print(temp(extraTeste))
                 chances = 3
-                return chances
+                return chances, valorAntigo
     elif chances == 3:
         print("Hoje é um dia em que sua sorte está dormindo! Mas lembre-se: o que chamamos por sorte é quando oportunidade e habilidade se encontram!\nAté a próxima!")
         print(f"O valor que o jogo escolheu foi {valorResposta}")
         chances = 4
         print("Fim do Jogo!")
-        return chances
+        return chances, valorAntigo
