f17cd4d2107b6824f0c279b7f451334c21030534
Djaysel Pessôa <djaysel@outlook.com>
2021-08-18 14:07:22 -0300

    Create defSorteAcerto.py

---
+++ folder1/defSorteAcerto.py
@@ -0,0 +1,11 @@
+def acerto(chances):
+    chances = chances
+
+    if chances == 1:
+        print("Vitória!\nSeu nível de sorte está magnífico hoje! Número certo na primeira jogada! Parabéns e aproveite!")
+    elif chances == 2:
+        print("Vitória!\nVocê pode até achar que não tem sorte, mas a verdade é que só precisou de duas tentativas para acertar! Parabéns!")
+    elif chances == 3:
+        print("Vitória!\nSua sorte não está na melhor forma, mas pense que só precisou de três tentativas! Parabéns!")
+
+    return ""
