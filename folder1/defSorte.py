7c52402ced90073b412ce76521932f7740dd903a
Djaysel Pessôa <djaysel@outlook.com>
2021-08-18 19:07:33 -0300

    Update defSorte.py

--- folder1/defSorte.py
+++ folder1/defSorte.py
@@ -1,9 +1,9 @@
 def temp(extraTeste):
     extraTeste = extraTeste
     if extraTeste >= 50:
-        print("Está muito frio!")
-    elif 50 > extraTeste >= 30:
-        print("Está morno!")
-    elif 30 > extraTeste >= 1:
-        print("Está quente!")
+        print("Dica: Está muito frio!")
+    elif 50 > extraTeste >= 25:
+        print("Dica: Está morno!")
+    elif 25 > extraTeste >= 1:
+        print("Dica: Está quente!")
     return ""
